e3fe0774681dc5ce7d87af842e87cbe5680db8a8
Leo Wiemelt <98919811+leowiemelt@users.noreply.github.com>
2026-04-25 16:46:08 -0700

    dark mode

--- global.js
+++ global.js
@@ -26,5 +26,17 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   // next step: create link and add it to nav
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+   let a = document.createElement('a');
+   a.href = url;
+   a.textContent = title;
+   nav.append(a);
 }
+
+if (a.host === location.host && a.pathname === location.pathname) {
+  a.classList.add('current');
+}
+
+a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
+);
