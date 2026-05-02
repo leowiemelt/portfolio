27c1a363cf6b0d6d7261b74f28a4a465e70293f1
Leo Wiemelt <98919811+leowiemelt@users.noreply.github.com>
2026-05-01 20:42:25 -0700

    fixed js

--- projects/projects.js
+++ projects/projects.js
@@ -1,18 +1,10 @@
 import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-let total = 0;
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
+
 let data = [
   { value: 1, label: 'apples' },
   { value: 2, label: 'oranges' },
@@ -21,8 +13,8 @@ let data = [
   { value: 5, label: 'limes' },
   { value: 5, label: 'cherries' },
 ];
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -42,4 +34,3 @@ data.forEach((d, idx) => {
         .attr('class', 'legend-item')
         .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
-
