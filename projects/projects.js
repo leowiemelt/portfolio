b7a4d1da21af9a9381a517c4be6cb579210bfb06
Leo Wiemelt <98919811+leowiemelt@users.noreply.github.com>
2026-05-01 18:29:21 -0700

    step 1 done

--- projects/projects.js
+++ projects/projects.js
@@ -4,25 +4,21 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
-let data = [1, 2];
 let total = 0;
 for (let d of data) {
   total += d;
 }
 let angle = 0;
-let arcData = [];
-
 for (let d of data) {
   let endAngle = angle + (d / total) * 2 * Math.PI;
   arcData.push({ startAngle: angle, endAngle });
   angle = endAngle;
 }
-let colors = ['gold', 'purple'];
-let arcs = arcData.map((d) => arcGenerator(d));
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d))
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 arcs.forEach((arc, idx) => {
-  d3.select('svg').append('path').attr('d', arc).attr('fill', colors[idx]);
+  d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
 });
