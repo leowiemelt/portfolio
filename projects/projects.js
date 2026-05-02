e41636ae47b1669474e7ca2cfac4515a2dc1ecce
Leo Wiemelt <98919811+leowiemelt@users.noreply.github.com>
2026-05-01 18:42:47 -0700

    finish 2.2

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,6 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let total = 0;
 for (let d of data) {
   total += d;
@@ -14,11 +13,33 @@ for (let d of data) {
   arcData.push({ startAngle: angle, endAngle });
   angle = endAngle;
 }
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d))
+let arcs = arcData.map((d) => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
 arcs.forEach((arc, idx) => {
-  d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
 });
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend.append('li')
+        .attr('style', `--color:${colors(idx)}`)
+        .attr('class', 'legend-item')
+        .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+});
+
